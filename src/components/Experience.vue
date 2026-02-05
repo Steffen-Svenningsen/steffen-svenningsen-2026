1ffaad91f06f43a1dd0fc38536092c6844cd89b1
Steffen-Svenningsen <stef7625@gmail.com>
2026-02-05 12:53:34 +0100

    feature: border styling

--- src/components/Experience.vue
+++ src/components/Experience.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="flex justify-between items-start py-4 border-b border-gray-200">
+    <div class="flex justify-between items-start border rounded-lg p-4 my-2 border-gray-200">
         <div class="relative">
             <span v-if="tag" class="rounded-md border border-slate-700 py-1 px-2 text-xs text-slate-700 inline-block mb-2 sm:absolute sm:top-0 sm:-right-2 sm:translate-x-full">{{ tag }}</span>
             <h3 class="text-lg">{{ title }}</h3>
